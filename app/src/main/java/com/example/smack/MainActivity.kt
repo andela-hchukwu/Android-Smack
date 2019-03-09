c2cf5e5a193d939a19e461d999066cc3b4124952
Henry Chukwu <henry.chukwu@andela.com>
2019-03-09 04:04:09 +0100

    mainactivity ui set up

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -34,5 +31,11 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+    fun loginBtnNavClicked(view: View) {}
+
+    fun addChannelClicked(view: View) {}
+
+    fun sendMessageBtnClicked(view: View) {}
 
 }
