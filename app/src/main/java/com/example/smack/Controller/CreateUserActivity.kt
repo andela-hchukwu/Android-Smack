0540faf7f2feedd6f578810b1fccf73c37b603a1
Henry Chukwu <henry.chukwu@andela.com>
2019-04-03 13:01:15 +0100

    login a user

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -35,9 +35,16 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserBtnClicked(view: View) {
-        AuthService.registerUser(this, "j@j.com", "123456") { complete ->
-            if (complete) {
-
+        val email = createEmailText.text.toString()
+        val password = createPasswordText.text.toString()
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess) {
+                AuthService.loginUser(this, email, password) { loginSuccess ->
+                    if (loginSuccess) {
+                        println(AuthService.authToken)
+                        println(AuthService.userEmail)
+                    }
+                }
             }
         }
     }
